47d7b945ea54b85432c7a25b5a2ad64f3b41b46e
Gabriel Malet <gabrielmalet@gmail.com>
2018-06-20 11:47:38 +0200

    Test new alpine image for all needed stuff

---
+++ resources/entrypoint.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -e
+
+# Check private key
+if [ -z "${SSH_PRIVATE_KEY}" ]; then
+	echo >&2 'warning: missing SSH_PRIVATE_KEY variable'
+else
+    eval $(ssh-agent -s)
+    ssh-add <(echo "${SSH_PRIVATE_KEY}")
+    unset SSH_PRIVATE_KEY
+fi
+
+# Replace nexus variables
+sed -i \
+    -e "s/##deployer_user##/${NEXUS_USER}/g" \
+    -e "s/##deployer_pwd##/${NEXUS_PASSWORD}/g" \
+    /root/.m2/settings.xml
+
+exec "$@"
